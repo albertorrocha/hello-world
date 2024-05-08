a8aa22dc9f8ee7cc085e6bc16943b7f65306bb0d
albertorrocha <albertorrocha023@gmail.com>
2024-05-08 15:27:14 -0300

    git upload

---
+++ wfUploadFiles/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Data = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Mensagem = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(221, 135)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Atualizando"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(185, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 39)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "13:25:40"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.White
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 99)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(500, 23)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'ListView1
+        '
+        Me.ListView1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.Data, Me.Mensagem})
+        Me.ListView1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListView1.ForeColor = System.Drawing.Color.White
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(547, 4)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(663, 200)
+        Me.ListView1.TabIndex = 8
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'Data
+        '
+        Me.Data.Text = "Data"
+        Me.Data.Width = 138
+        '
+        'Mensagem
+        '
+        Me.Mensagem.Text = "Mensagem"
+        Me.Mensagem.Width = 517
+        '
+        'Timer3
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(1222, 206)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Upload"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Data As ColumnHeader
+    Friend WithEvents Mensagem As ColumnHeader
+    Friend WithEvents Timer3 As Timer
+End Class
---
+++ wfUploadFiles/Form1.vb
@@ -0,0 +1,183 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.IO.Packaging
+Imports System.Net.Mail
+Imports wfUploadFiles.ERPDD.DO.Service
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Timer1 = New Timer
+        Timer1.Enabled = True
+        Timer1.Interval = 3600000
+
+        Timer2 = New Timer
+        Timer2.Enabled = True
+        Timer2.Interval = 1000
+
+        Timer3 = New Timer
+        Timer3.Enabled = True
+        Timer3.Interval = 90000
+
+        Label1.Text = Format(Date.Now, "HH:mm:ss")
+        Label2.Text = ""
+    End Sub
+    Private Sub Starting(sender As Object, e As EventArgs)
+        Label2.Text = "Última Atualização " + Format(Date.Now, "dd/MM/yyyy HH:mm").ToString()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim data As DateTime
+        '------------------------
+        data = Date.Now
+        Label1.Text = Format(Date.Now, "HH:mm:ss")
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = 0
+        If (data.Hour) = 2 Then
+            Starting(sender, e)
+            'If (data.DayOfWeek) = 5 Then
+            CopyFiles()
+            'End If
+        End If
+    End Sub
+    Private Function CompactFile(ByRef Msg As String, ByVal Caminho As String) As Boolean
+        Try
+            ZipFile.CreateFromDirectory(Caminho, Caminho & ".zip")
+            Return True
+        Catch ex As Exception
+            Msg = ex.Message
+            Return False
+        End Try
+    End Function
+    Private Function UploadFiles(ByRef Msg As String, ByVal Caminho As String, ByVal NomeArquivo As String) As Boolean
+        Try
+            Dim Request As System.Net.FtpWebRequest
+            Dim Strz As System.IO.Stream
+            Dim File() As Byte
+
+            Dim FTP As String
+            Dim User As String
+            Dim Pass As String
+            '----------------------------------------
+            FTP = ""
+            User = ""
+            Pass = ""
+            '
+            FTP += "PASTA/" + NomeArquivo.ToString()
+            Request = DirectCast(System.Net.WebRequest.Create(FTP), System.Net.FtpWebRequest)
+            Request.Credentials = New System.Net.NetworkCredential(User, Pass)
+            Request.Method = System.Net.WebRequestMethods.Ftp.UploadFile
+
+            File = System.IO.File.ReadAllBytes(Caminho + NomeArquivo)
+
+            Strz = Request.GetRequestStream()
+            Strz.Write(File, 0, File.Length)
+            Strz.Close()
+            Strz.Dispose()
+            Msg = "Upload Completed"
+            Return True
+        Catch ex As Exception
+            Msg = ex.Message.ToString() & vbCrLf
+            Msg += "Data: " + Format(Date.Now, "dd/MM/yy HH:mm")
+            If Not System.Diagnostics.Debugger.IsAttached Then
+                EnviaEmailSMTP(Msg, "teste@teste.com.br", "teste@teste.com.br", "", "Upload - " + ex.Message.ToString(), 1, 1, Nothing)
+            End If
+            Return False
+        End Try
+    End Function
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Label1.Text = Format(Date.Now, "HH:mm:ss")
+    End Sub
+    Private Sub CopyFiles()
+        Try
+            Dim Destino As String
+            Dim Caminho As String
+            Dim Arquivos() As String
+            Dim NomeArquivo As String
+            '-----------------------
+            Caminho = "PASTA\"
+            Destino = Caminho & Format(Date.Now, "yyyyMMdd")
+            Directory.CreateDirectory(Destino)
+            Arquivos = Directory.GetFiles(Caminho, "*")
+            '
+            For i As Integer = 0 To UBound(Arquivos)
+                '
+                NomeArquivo = Arquivos(i).Replace(Caminho, "").Trim()
+                System.IO.File.Copy(Arquivos(i), Destino & "\" & NomeArquivo)
+                System.IO.File.Delete(Arquivos(i))
+            Next
+            '
+            Arquivos = Directory.GetDirectories(Caminho)
+            If Arquivos.Length >= 8 Then
+                For i As Integer = 0 To 0
+                    System.IO.Directory.Delete(Arquivos(i), True)
+                Next
+            End If
+            '
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub CopyFolder(ByVal Caminho As String, ByVal Destino As String, ByVal Extensao As String)
+        Dim Arquivos() As String
+        Dim NomeArquivo As String
+        '---------------------------
+
+        Arquivos = Directory.GetFiles(Caminho, "*." & Extensao & "")
+        Directory.CreateDirectory(Destino)
+        '
+        For i As Integer = 0 To UBound(Arquivos)
+            '
+            NomeArquivo = Arquivos(i).Replace(Caminho, "").Trim()
+            System.IO.File.Copy(Arquivos(i), Destino & "\" & NomeArquivo)
+            System.IO.File.Delete(Arquivos(i))
+        Next
+    End Sub
+    Private Function EnviaEmailSMTP(ByVal Mensagem As String,
+                          ByVal Remetente As String,
+                          ByVal Destinatario As String,
+                          ByVal CopiaOculta As String,
+                          ByVal Titulo As String,
+                          ByVal id_filial As String,
+                          ByVal id_pedido As String,
+                          ByVal Arquivos As List(Of String)) As Boolean
+        Try
+            EnviaEmailSMTP = True
+            'Cria uma instância da classe MailMessage
+            Dim mail As New MailMessage()
+            Dim vDest() As String = Split(Destinatario, ";")
+            For i As Integer = 0 To UBound(vDest)
+                If Len(vDest(i)) > 0 Then mail.[To].Add(vDest(i)) 'destino
+            Next
+            If Len(CopiaOculta) > 0 Then mail.[Bcc].Add(CopiaOculta) 'Cópia Oculta
+            mail.From = New MailAddress(Remetente, "REMETENTE") 'Remetente
+            mail.Subject = Titulo ''assunto
+            mail.Body = Mensagem '  'corpo do email (texto)
+            mail.IsBodyHtml = True ''é html ?
+            Try
+                For i As Integer = 0 To Arquivos.Count - 1
+                    mail.Attachments.Add(New Attachment(Arquivos(i)))
+                Next
+            Catch ex As Exception : End Try
+            '-------------------------------------------
+            'cria instãncia STMP
+            Dim smtp As New SmtpClient()
+            smtp.Host = "smtp.gmail.com" ' 'define o servidor SMTP
+            smtp.Credentials = New System.Net.NetworkCredential("teste@teste.com.br", "pass") 'Dados do seu servidor SMTP | Suas credenciais SMTP
+            smtp.EnableSsl = True 'habilita o envio via SSL
+            smtp.Send(mail) 'envia o email
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Dim url As String = "/webhook"
+        ' Criar uma instância do WebClient
+        Dim client As New System.Net.WebClient
+        Try
+            ' Fazer a solicitação GET e obter a resposta como uma string
+            client.DownloadString(url)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
